fb22e2951663dc5ad298536a7c7de994a147df84
Rob Gulewich <robert.gulewich@joyent.com>
2014-08-25 11:57:52 -0700

    TOOLS-607 I for one, welcome my new open source license

--- test/lib/log.js
+++ test/lib/log.js
@@ -1,7 +1,15 @@
 /*
- * Copyright (c) 2014, Joyent, Inc. All rights reserved.
- *
- * bunyan logger for tests
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/*
+ * Copyright (c) 2014, Joyent, Inc.
+ */
+
+/*
+ * bunyan logger for tests: it's better than bad, it's good!
  */
 
 var bunyan = require('bunyan');
